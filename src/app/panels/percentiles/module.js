4e7fe99a57553c15231afaece7879efaa9b1a8c1
饶琛琳 <raochenlin@raochenlindeMacBook-Air.local>
2014-07-14 13:00:11 +0800

    change to use 25% and 75% as default, so we can cal upper threshold of box and whisker

--- src/app/panels/percentiles/module.js
+++ src/app/panels/percentiles/module.js
@@ -50,7 +50,7 @@ define([
       description: 'A percentiles panel for displaying aggregations using the Elastic Search percentiles aggregation query.'
     };
 
-    $scope.modes = ['50','75','90','95','99'];
+    $scope.modes = ['25','50','75','90','95','99'];
 
     var defaults = {
       queries     : {
@@ -67,8 +67,8 @@ define([
       value_name: 'Value',
       spyable     : true,
       show: {
+        '25': true,
         '75': true,
-        '90': true,
         '95': true,
         '99': true,
       }
