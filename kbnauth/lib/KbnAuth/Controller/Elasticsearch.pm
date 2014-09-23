d3edddd65eb7fe3acc89b6dccef674dcc1cd762c
chenryn <rao.chenlin@gmail.com>
2014-09-23 11:27:25 +0800

    support `[logstash-]YYYY`, `[logstash-]YYYY.MM` and `_all` for regexp

--- kbnauth/lib/KbnAuth/Controller/Elasticsearch.pm
+++ kbnauth/lib/KbnAuth/Controller/Elasticsearch.pm
@@ -2,55 +2,55 @@ package KbnAuth::Controller::Elasticsearch;
 use Mojo::Base 'Mojolicious::Controller';
 
 sub proxy {
-  my $self  = shift;
-  my $preq  = $self->req->clone;
-  my $esurl = $self->users->server( $self->session('user') )
-    // $self->config('eshost');
-  my ( $eshost, $esport ) = $esurl =~ m!^(?:https?://)?([^:]+)(?::(\d+))?!;
-  $eshost ||= 'localhost';
-  $esport ||= 9200;
-  $preq->url->scheme('http')->host($eshost)->port($esport);
-  my $tx = Mojo::Transaction::HTTP->new( req => $preq );
-  $self->ua->start($tx);
-  $self->render( json => $tx->res->json );
+    my $self  = shift;
+    my $preq  = $self->req->clone;
+    my $esurl = $self->users->server( $self->session('user') )
+      // $self->config('eshost');
+    my ( $eshost, $esport ) = $esurl =~ m!^(?:https?://)?([^:]+)(?::(\d+))?!;
+    $eshost ||= 'localhost';
+    $esport ||= 9200;
+    $preq->url->scheme('http')->host($eshost)->port($esport);
+    my $tx = Mojo::Transaction::HTTP->new( req => $preq );
+    $self->ua->start($tx);
+    $self->render( json => $tx->res->json );
 }
 
 sub auth_dashboards {
-  my $self = shift;
-  return $self->render(
-    json => { status => 404, error => 'DashboardsNoPermissionException' } )
-    unless $self->param('kbnidx') eq 'kibana-int-' . $self->session('user');
-  $self->proxy;
+    my $self = shift;
+    return $self->render(
+        json => { status => 404, error => 'DashboardsNoPermissionException' } )
+      unless $self->param('kbnidx') eq 'kibana-int-' . $self->session('user');
+    $self->proxy;
 }
 
 sub auth_proxy {
-  my $self = shift;
-  my %seen = ();
-  print $self->param('index');
-  my @indices =
-    grep { not $seen{$_}++ }
-    grep { s/-\d{4}\.\d{2}\.\d{2}$// } split( /,|\%2C/, $self->param('index') );
-  return $self->render(
-    json => { status => 404, error => 'IndexNoPermissionException' } )
-    unless $self->users->permiss( $self->session('user'), \@indices );
-  $self->proxy;
+    my $self = shift;
+    my %seen = ();
+    my @indices =
+      grep { not $seen{$_}++ }
+      grep { s/-\d{4}(?:\.\d{2}\.(?:\d{2})?)?$// || $_ }
+      split( /,|\%2C/, $self->param('index') );
+    return $self->render(
+        json => { status => 404, error => 'IndexNoPermissionException' } )
+      unless $self->users->permiss( $self->session('user'), \@indices );
+    $self->proxy;
 }
 
 sub hidden_nodes {
-  my $self       = shift;
-  my $local_addr = $self->tx->local_address;
-  my $local_port = $self->tx->local_port;
-  $self->render(
-    json => {
-      nodes => {
-        "web" => {
-          version      => '1.1.1',
-          http_address => "inet[/$local_addr:$local_port]",
-          ip           => "$local_addr",
+    my $self       = shift;
+    my $local_addr = $self->tx->local_address;
+    my $local_port = $self->tx->local_port;
+    $self->render(
+        json => {
+            nodes => {
+                "web" => {
+                    version      => '1.1.1',
+                    http_address => "inet[/$local_addr:$local_port]",
+                    ip           => "$local_addr",
+                }
+            }
         }
-      }
-    }
-  );
+    );
 }
 
 1;
