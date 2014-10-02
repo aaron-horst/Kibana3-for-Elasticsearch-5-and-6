a0c30632d0ba748d5f678e6f6b71a337aedda7bf
Rashid Khan <rashidkpc@gmail.com>
2014-10-02 10:06:16 -0700

    Fix minification bug

--- src/app/app.js
+++ src/app/app.js
@@ -74,7 +74,8 @@ function (angular, $, _, appLevelRequire) {
 
   app.config(function ($routeProvider, $controllerProvider, $httpProvider, $compileProvider, $filterProvider, $provide) {
 
-    $httpProvider.interceptors.push(function($location) {
+
+    $httpProvider.interceptors.push(['$location', function($location) {
       return {
         responseError: function(resp) {
           if (resp.status === 0) {
@@ -83,7 +84,7 @@ function (angular, $, _, appLevelRequire) {
           }
         }
       };
-    });
+    }]);
 
     $routeProvider
       .when('/connectionFailed', {
