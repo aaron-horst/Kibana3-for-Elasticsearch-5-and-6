523225466bda87e3a8b6c7d384ad686e9a5f3193
aaron-horst <aaron@aaronhorst.com>
2016-10-25 11:06:02 -0400

    Add in support for other terms aggregation values

--- src/app/panels/terms/module.js
+++ src/app/panels/terms/module.js
@@ -413,13 +413,17 @@ function (angular, app, _, $, kbn) {
             k = k + 1;
           });
 
-          // scope.data.push({label:'Missing field',
-          //   data:[[k,scope.results.facets.terms.missing]],meta:"missing",color:'#aaa',opacity:0});
-          //
-          // if(scope.panel.tmode === 'terms') {
-          //   scope.data.push({label:'Other values',
-          //     data:[[k+1,scope.results.facets.terms.other]],meta:"other",color:'#444'});
-          // }
+          
+          if (scope.panel.tmode === 'terms') {
+              scope.data.push({
+                  label: 'Other values',
+                  data: [[k + 1, scope.results.aggregations.terms.sum_other_doc_count]],
+                  meta: "other",
+                  color: '#444'
+              });
+          }
+
+         
         }
 
         // Function for rendering panel
